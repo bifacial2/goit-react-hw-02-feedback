5061d0c80410419ebd27bd4711a8e50648d0a18a
Stanislav Fedets <fedets.stanislav@gmail.com>
2022-08-17 12:51:23 +0200

    step 2

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Controls from '../Controls/Controls';
+import Statistics from '../Statistics/Statistics';
 
 class Counter extends React.Component {
   static defaultProps = {
@@ -54,29 +56,19 @@ class Counter extends React.Component {
     return (
       <div>
         <h1 className="title">Please live feedback</h1>
-        <div>
-          <button className="good" type="button" onClick={this.addGoodReview}>
-            Good
-          </button>
-          <button
-            className="neutral"
-            type="button"
-            onClick={this.addNeutralReview}
-          >
-            Neutral
-          </button>
-          <button className="bad" type="button" onClick={this.addBadReview}>
-            Bad
-          </button>
-        </div>
+        <Controls
+          addGoodReview={this.addGoodReview}
+          addNeutralReview={this.addNeutralReview}
+          addBadReview={this.addBadReview}
+        />
         <h2 className="statistics">Statistics</h2>
-        <ul>
-          <li>Good: {this.state.good}</li>
-          <li>Neutral: {this.state.neutral}</li>
-          <li>Bad: {this.state.bad}</li>
-          <li>Total: {this.state.total}</li>
-          <li>Positive Feedback: {this.state.positiveFeedback}%</li>
-        </ul>
+        <Statistics
+          good={this.state.good}
+          neutral={this.state.neutral}
+          bad={this.state.bad}
+          total={this.state.total}
+          positivePercentage={this.state.positiveFeedback}
+        />
       </div>
     );
   }
